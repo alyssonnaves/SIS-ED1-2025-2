c0de28aadf83ce74ff382ceb0a9f919be647ab7c
Alysson Naves <alysson.naves@ectare.com.br>
2025-11-24 21:50:02 -0300

    aula 24/11

--- dicionario-json/exemplo2.py
+++ dicionario-json/exemplo2.py
@@ -8,17 +8,18 @@ def produto_estoque(nome, valor, quantidade):
     }
 
 def relatorio_estoque(estoque):
-    print("_"*50)
-    print("nome_prod\t|valor_prod\t|qtd_prod") #\t -> tab
+    print("_"*70)
+    print("nome_prod\t|valor_prod\t|qtd_prod\t|valor_parcial") #\t -> tab
     for i in range(len(estoque)):
         prod = estoque[i]
         # print(i)
         # print(prod)
         # nome = prod["nome_produto"]
         nome = prod.get("nome_prod","indefinido") # valor padrao -
-        valor = prod.get("valor_prod", "R$ 0.00")
+        valor = prod.get("valor_prod", 0)
         quantidade = prod.get("qtd_prod", 0)
-        print(f"{nome:<15}\t|R$ {valor:<6.2f}\t|{quantidade}")
+        total = valor*quantidade
+        print(f"{nome:<15}\t|R$ {valor:<6.2f}\t|{quantidade:<7}\t|R$ {total:<6.2f}")
 
 def salvar_json(estoque):
     dados = {"registros": estoque}
@@ -44,8 +45,18 @@ def teste_escrita():
     relatorio_estoque(estoque)
     salvar_json(estoque)
 
-if __name__ == "__main__":
-#    teste_escrita()
-    dados_estoque = ler_json()
-    relatorio_estoque(dados_estoque.get("registros",[]))
+def calcular_total_itens_estoque(estoque):
+    total = 0
+    for item in estoque:
+        qtd = item.get("qtd_prod", 0)
+        total = total + qtd
+    return total
 
+if __name__ == "__main__":
+    # teste_escrita()
+    dados_estoque = ler_json()
+    estoque = dados_estoque.get("registros",[])
+    relatorio_estoque(estoque)
+    total_itens = calcular_total_itens_estoque(estoque)
+    print(f"-----TOTAL-----")
+    print(total_itens)
